474b5aa51519d855d8848924cce3a4dcb4319fd8
jadsonmarliere <jds.mdo@gmail.com>
2019-04-14 14:56:44 -0300

    fix connection database

--- connectionDB.py
+++ connectionDB.py
@@ -1,10 +1,30 @@
-from pymongo import MongoClient
+from random import *
+from pymongo import *
 
-client = MongoClient('localhost', 27017)
 
-db = client['database']
+client = MongoClient('localhost', 27017) # conectaem um cliente do mongoDB que esta funcionando em sua máquina
+db = client['ERP'] #acessa o database
+books = db['AleatoryNumbers'] #acessa a coleção dentro do banco de dados
 
-books = db['books']
-
+# retorna os valores dentro da coleção AleatoryNumbers
 for b in books.find():
     print (b)
+
+x = 0
+k = 0
+
+seed()
+db = client.ERP  # conecta com a bases de dados ERP
+col = db.AleatoryNumbers  # recupera a tabela que queremos usar
+
+
+# gera numeros aleatorios e popula a coleção AleatoryNumbers
+while (x<200):
+
+    k = randrange(0, 200)
+    col.insert_one({"intnum": k})
+
+    x = x + 1
+
+
+
