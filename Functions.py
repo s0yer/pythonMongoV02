ecdfe08f9728ad65f480a350972912dddc4e61ac
jadsonmarliere <jds.mdo@gmail.com>
2019-04-17 22:54:38 -0300

    method connectionDB implementation

--- Functions.py
+++ Functions.py
@@ -3,7 +3,16 @@ from pymongo import *
 
 # this function will be implemented later
 def connectionDB():
-    z=0
+    client = MongoClient('localhost', 27017)  # conectaem um cliente do mongoDB que esta funcionando em sua máquina
+    db = client.ERP  # conecta com a bases de dados ERP
+    col = db.Numbers  # recupera a tabela que queremos usar
+    return col
+
+def makeBooks():
+    client = MongoClient('localhost', 27017)  # conectaem um cliente do mongoDB que esta funcionando em sua máquina
+    db = client['ERP']  # acessa o database
+    books = db['Numbers']  # acessa a coleção dentro do banco de dados
+    return books
 
 def choiceUser():
     choice = input('Sua escolha: ')
@@ -11,19 +20,11 @@ def choiceUser():
 
 def showData():
 
-    client = MongoClient('localhost', 27017)  # conectaem um cliente do mongoDB que esta funcionando em sua máquina
-    db = client['ERP']  # acessa o database
-    books = db['Numbers']  # acessa a coleção dentro do banco de dados
-
     # retorna os valores dentro da coleção AleatoryNumbers
-    for b in books.find():
+    for b in makeBooks().find():
         print(b)
 
 def createNumbersDatabase():
-
-    client = MongoClient('localhost', 27017)  # conectaem um cliente do mongoDB que esta funcionando em sua máquina
-    db = client.ERP  # conecta com a bases de dados ERP
-    col = db.Numbers  # recupera a tabela que queremos usar
 
     x = 0
     seed()
@@ -31,25 +32,18 @@ def createNumbersDatabase():
     # gera numeros aleatorios e popula a coleção AleatoryNumbers
     while (x < 200):
         k = randrange(0, 200)
-        col.insert_one({"id": x , "intnum": k })
+        connectionDB().insert_one({"id": x , "intnum": k })
 
         x = x + 1
 
 def deleteData():
-    client = MongoClient('localhost', 27017)  # conectaem um cliente do mongoDB que esta funcionando em sua máquina
-    db = client.ERP  # conecta com a bases de dados ERP
-    col = db.Numbers  # recupera a tabela que queremos usar
 
     x = 0
     while (x < 200):
-        col.delete_one({"id": x})
+        connectionDB().delete_one({"id": x})
         x = x + 1
 
 def updateData():
-    client = MongoClient('localhost', 27017)  # conectaem um cliente do mongoDB que esta funcionando em sua máquina
-    db = client.ERP  # conecta com a bases de dados ERP
-    col = db.Numbers  # recupera a tabela que queremos usar
 
-    books = db['Numbers']  # acessa a coleção dentro do banco de dados
-    for b in books.find():
-        col.update({"intnum": 7},{"$set":{"intnum": 300}})
+    for b in makeBooks().find():
+        connectionDB().update({"intnum": 100},{"$set":{"intnum": 300}})
