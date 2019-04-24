0704565ddd0a036a269dd3c32c95df607aef6a0c
jadsonmarliere <jds.mdo@gmail.com>
2019-04-23 22:06:59 -0300

    GUI fixes

--- Application.py
+++ Application.py
@@ -8,12 +8,53 @@ class Application:
         self.msg = Label(self.widget1, text="CRUD Program")
         self.msg.pack()
 
-        self.sair = Button(self.widget1)
-        self.sair["text"] = "Exit"
-        self.sair["font"] = ("Arial", "9")
-        self.sair["width"] = 6
-        self.sair["command"] = self.widget1.quit
-        self.sair.pack()
+        self.create = Button(self.widget1)
+        self.create["text"] = "Create numbers Database"
+        self.create["font"] = ("Arial", "9")
+        self.create["width"] = 23
+        self.create["command"] = self.createNumDB
+        self.create.pack()
+
+        self.delete = Button(self.widget1)
+        self.delete["text"] = "Delete Data"
+        self.delete["font"] = ("Arial", "9")
+        self.delete["width"] = 16
+        self.delete["command"] = self.delNumDB
+        self.delete.pack()
+
+        self.show = Button(self.widget1)
+        self.show["text"] = "Show Database"
+        self.show["font"] = ("Arial", "9")
+        self.show["width"] = 16
+        self.show["command"] = self.showDB
+        self.show.pack()
+
+        self.update = Button(self.widget1)
+        self.update["text"] = "Update Data"
+        self.update["font"] = ("Arial", "9")
+        self.update["width"] = 16
+        self.update["command"] = self.updateDB
+        self.update.pack()
+
+        self.exit = Button(self.widget1)
+        self.exit["text"] = "Exit GUI"
+        self.exit["font"] = ("Arial", "9")
+        self.exit["width"] = 16
+        self.exit["command"] = self.widget1.quit
+        self.exit.pack()
+
+
+    def createNumDB(self):
+        createNumbersDatabase()
+
+    def delNumDB(self):
+        deleteData()
+
+    def showDB(self):
+        showData()
+
+    def updateDB(self):
+        updateData()
 
 root = Tk()
 Application(root)
